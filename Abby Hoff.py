a6943159705a0472a7bfb4cd98e19c3f3d277ecb
abigailhoff <80072059+abigailhoff@users.noreply.github.com>
2021-03-04 16:58:01 -0500

    Add files via upload

---
+++ Abby Hoff.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=float(input('chooose a value for a:\n'))
+b=float(input('choose a value for b:\n'))
+print('The a value is:',a)
+print('The b value is:',b)
+y=3*a**b-2*a/b+(6*(a-b))
+print('The output is:',y)
+
+
